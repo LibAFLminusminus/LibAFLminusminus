1e4e42e3e220b970cda38e3034ba91a79cf4f7c2
Romain Malmain <romain.malmain@pm.me>
2026-04-22 17:23:13 +0200

    stats

--- crates/libafl/src/controllers/aflpp.rs
+++ crates/libafl/src/controllers/aflpp.rs
@@ -1,3 +1,10 @@
+use std::{collections::HashMap, path::PathBuf, vec::Vec};
+
+pub struct MainClientController {
+    secondary_queues: Vec<PathBuf>,
+    last_evaluated: HashMap<PathBuf, Option<u64>>,
+}
+
 pub enum AflppController {
     Main(),
     Secondary(),
