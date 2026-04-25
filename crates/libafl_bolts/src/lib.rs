f6da8d89ce3b088a52e6437255c598e6f4dc69ec
Romain Malmain <romain.malmain@pm.me>
2026-04-25 18:22:48 +0200

    shm now can work without header

--- crates/libafl_bolts/src/lib.rs
+++ crates/libafl_bolts/src/lib.rs
@@ -54,12 +54,15 @@ extern crate std;
 #[doc(hidden)]
 pub extern crate alloc;
 
-pub mod shm;
 #[cfg(feature = "std")]
 pub use build_id2 as build_id;
 #[cfg(feature = "alloc")]
 pub use serde_anymap::anymap;
-pub use shm::{AnonShmBuilder, AnonShmReceiver, AnonShmSender, SharedMemory, SysVShm};
+pub mod shm;
+pub use shm::{
+    AnonShmBuilder, AnonShmReceiver, AnonShmSender, EmptyShmHeader, SharedMemory, ShmHeader,
+    SysVShm,
+};
 #[cfg(all(
     any(feature = "cli", feature = "frida_cli", feature = "qemu_cli"),
     feature = "std"
