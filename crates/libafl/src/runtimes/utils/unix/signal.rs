a0971912edba35ba76ef1fbfe869da85051b029d
Romain Malmain <romain.malmain@pm.me>
2026-04-25 15:57:23 +0200

    -no pipe for now, not really useful - print backtrace only once on target crash

--- crates/libafl/src/runtimes/utils/unix/signal.rs
+++ crates/libafl/src/runtimes/utils/unix/signal.rs
@@ -274,8 +274,6 @@ where
         panic::set_hook(Box::new(move |panic_info| unsafe {
             let signal_params = OsTerminationParams::Panic(panic_info);
 
-            old_hook(panic_info);
-
             let signal_handler: &mut Self = &mut *signal_handler_ptr.as_mut_ptr();
 
             let max_depth_reached = signal_handler.enter();
@@ -296,12 +294,14 @@ where
                 .map(|p| p.as_ref().in_fuzzing())
                 .unwrap_or(false)
             {
+                // not in a fuzzing run: use the default hook (includes RUST_BACKTRACE output)
+                old_hook(panic_info);
                 log::error!("Fuzzer panicked out of the fuzzing loop. This is a Fuzzer bug.");
                 libafl_bolts::os::exit(128 + SIGABRT);
                 return;
             }
 
-            // fuzzing in progress, propagate crash
+            // fuzzing in progress: print our own backtrace, skip the default hook
             log::error!("Target panicked: {panic_info}");
             let backtrace = Backtrace::force_capture();
             eprintln!("stack backtrace:\n{backtrace}");
