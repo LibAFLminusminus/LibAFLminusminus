2d38e0a31982df9a65fd21c9515a0165491bf95a
Dongjia Zhang <tokazerkje@outlook.com>
2026-05-06 18:04:10 +0200

    nyx barebones; still testing

---
+++ fuzzers/tutorials/baby_fuzzer_nyx/setup_libxml2.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -euxo pipefail
+
+# Check if afl-clang-fast exists in PATH
+if ! command -v afl-clang-fast &> /dev/null
+then
+    echo "afl-clang-fast not found. Cloning and compiling AFLplusplus..."
+	if [ ! -d AFLplusplus ]; then
+    	git clone https://github.com/AFLplusplus/AFLplusplus.git
+	fi
+    pushd AFLplusplus
+    make -j$(nproc)
+    popd
+    export CC="$(pwd)/AFLplusplus/afl-clang-fast"
+    export CXX="$(pwd)/AFLplusplus/afl-clang-fast++"
+else
+    echo "afl-clang-fast already exists in PATH."
+    export CC="afl-clang-fast"
+    export CXX="afl-clang-fast++"
+fi
+
+curl -C - https://gitlab.gnome.org/GNOME/libxml2/-/archive/v2.9.14/libxml2-v2.9.14.tar.gz --output libxml2-v2.9.14.tar.gz
+tar -xf ./libxml2-v2.9.14.tar.gz  --transform s/libxml2-v2.9.14/libxml2/ || exit
+pushd ./libxml2/ || exit
+./autogen.sh --enable-shared=no || exit
+make -j || exit
+popd || exit
+python3 "../../../crates/libafl_nyx/packer/packer/nyx_packer.py" \
+    ./libxml2/xmllint \
+    /tmp/nyx_libxml2 \
+    afl \
+    instrumentation \
+    -args "/tmp/input" \
+    -file "/tmp/input" \
+    --fast_reload_mode \
+    --purge || exit
+
+python3 ../../../crates/libafl_nyx/packer/packer/nyx_config_gen.py /tmp/nyx_libxml2/ Kernel || exit
---
+++ fuzzers/tutorials/baby_fuzzer_nyx/src/main.rs
@@ -0,0 +1,93 @@
+use libafl::{
+    corpus::{
+        schedulers::{NopScheduler, QueueScheduler},
+        Corpus, InMemoryCorpus, OnDiskCorpus, Scheduler,
+    },
+    feedbacks::{CrashFeedback, MaxMapFeedback},
+    inputs::{bytes::BytesContext, BytesInput},
+    launchers::StdLauncher,
+    monitors::SimpleMonitor,
+    mutators::{havoc_mutations, HavocScheduledMutator},
+    observers::StdMapObserver,
+    runtimes::RuntimeHandle,
+    simple::{SimpleController, SimpleWorker},
+    stages::StdMutationalStage,
+    states::StdState, Result,
+    Fuzzer, StdFuzzer, Worker,
+};
+use libafl_bolts::{rands::StdRand, tuples::tuple_list};
+use libafl_nyx::{executor::NyxExecutor, helper::NyxHelper, settings::NyxSettings};
+
+fn run_fuzzer<C, OC, SC>(
+    rt_handle: &mut RuntimeHandle<StdState<C, BytesInput, OC, SC>, SimpleWorker>,
+    state: &mut StdState<C, BytesInput, OC, SC>,
+) -> Result<()>
+where
+    C: Corpus<BytesInput>,
+    OC: Corpus<BytesInput>,
+    SC: Scheduler,
+{
+    // nyx stuff
+    let settings = NyxSettings::builder().cpu_id(0).parent_cpu_id(None).build();
+    let helper = NyxHelper::new("/tmp/nyx_libxml2/", settings).unwrap();
+    let observer =
+        unsafe { StdMapObserver::from_mut_ptr("trace", helper.bitmap_buffer, helper.bitmap_size) };
+
+    let input = BytesInput::new(b"100".to_vec());
+    let mut rand = StdRand::new();
+
+    // libafl stuff
+    let feedback = MaxMapFeedback::new(&observer);
+    let objective = CrashFeedback::new();
+
+    // switch monitor if you want
+    // let monitor = SimpleMonitor::new(|x|-> () {println!("{}",x)});
+    let mut executor = NyxExecutor::builder().build(helper, tuple_list!(observer));
+
+    let mutator = HavocScheduledMutator::new(havoc_mutations());
+    let mut stages = tuple_list!(StdMutationalStage::new(mutator));
+    let mut fuzzer = StdFuzzer::new(feedback, objective, &mut stages, &mut executor, state, rt_handle)?;
+
+
+    // start fuzz
+    fuzzer
+        .fuzz_loop(&mut stages, &mut executor, &mut rand, state, rt_handle)
+}
+
+pub fn main() -> Result<()> {
+    env_logger::init();
+
+    // The state creation closure.
+    let state_builder = |worker: &SimpleWorker| {
+        // A queue policy to get testcasess from the corpus
+        let scheduler = QueueScheduler::new();
+        let crash_dir = worker.workdir().create_dir("crashes")?;
+
+        // create a State from scratch
+        StdState::new(
+            // Corpus that will be evolved, we keep it in memory for performance
+            InMemoryCorpus::new(BytesContext, scheduler),
+            // Corpus in which we store solutions (crashes in this example),
+            // on disk so the user can get them after stopping the fuzzer
+            OnDiskCorpus::new(crash_dir, BytesContext, NopScheduler).unwrap(),
+        )
+    };
+
+    // The launcher supervises the fuzzer and communicates with the workers.
+    let controller = SimpleController::builder()
+        .worker_stdout(None)
+        .worker_stderr(None)
+        .overwrite(true)
+        .build()?;
+
+    // The monitor tracks the fuzzing current status.
+    let monitor = SimpleMonitor::new();
+
+    // Launch the fuzzer
+    StdLauncher::builder()?
+        .controller(controller)
+        .monitor(monitor)
+        .state_builder(state_builder)
+        .build_forkserver(run_fuzzer)?
+        .launch()
+}
