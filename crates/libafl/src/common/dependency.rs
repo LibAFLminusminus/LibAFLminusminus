1d0409d86186c4df82fa89888b16d4fc06664d3e
Romain Malmain <romain.malmain@pm.me>
2026-05-11 17:19:45 +0200

    dependency warnings

--- crates/libafl/src/common/dependency.rs
+++ crates/libafl/src/common/dependency.rs
@@ -10,17 +10,26 @@ use libafl_bolts::{
 
 use crate::states::add_named_metadata_checked;
 
+/// Dependency registrator.
+///
+/// Used to register types and metadata used during the fuzzing run.
+#[derive(Clone, Debug)]
 pub struct Registrator {
     map: NamedSerdeAnyMap,
     types: HashSet<&'static str>,
 }
 
+/// Dependency checker.
+///
+/// Used to check all dependencies are correctly met.
+#[derive(Clone, Debug)]
 pub struct CompatibilityChecker {
     map: NamedSerdeAnyMap,
     types: HashSet<&'static str>,
 }
 
 impl Registrator {
+    /// Create a new [`Registrator`]
     pub fn new(state_metadata: NamedSerdeAnyMap) -> Self {
         Self {
             map: state_metadata,
@@ -28,20 +37,24 @@ impl Registrator {
         }
     }
 
+    /// Register a new metadata.
     pub fn register_md<T: SerdeAny>(&mut self, name: String, value: T) {
         add_named_metadata_checked::<T>(&mut self.map, &name, value)
             .expect(format!("Addind same metadata twice: {name}").as_str())
     }
 
+    /// Register a new metadata, with its default value.
     pub fn register_md_default<T: Default + SerdeAny>(&mut self, name: String) {
         add_named_metadata_checked::<T>(&mut self.map, &name, T::default())
             .expect(format!("Addind same metadata twice: {name}").as_str())
     }
 
+    /// Register a new type.
     pub fn register_ty<T: ?Sized>(&mut self) -> bool {
         self.types.insert(any::type_name::<T>())
     }
 
+    /// Finish the registration, and get the [`CompabilityChecker`].
     pub fn finish(self) -> CompatibilityChecker {
         CompatibilityChecker {
             map: self.map,
@@ -51,15 +64,21 @@ impl Registrator {
 }
 
 impl CompatibilityChecker {
+    /// Does the checker contains the given type?
     pub fn contains<T>(&self) -> bool {
         self.types.contains(any::type_name::<T>())
     }
 
+    /// Get the final [`NamedSerdeAnyMap`], produced when all checks are done.
     pub fn finish(self) -> NamedSerdeAnyMap {
         self.map
     }
 }
 
+/// Trait for objects with dependencies.
+///
+/// Every main `LibAFLmm` objects must implement it.
+/// That way, it is possible to detect missing types and metadata configuration early.
 pub trait DependencyResolver {
     /// Register in the resolver the metadata necessary during runtime.
     ///
