f1c9e4823de07c2ee051b68d5b99194b6a070a09
Romain Malmain <romain.malmain@pm.me>
2026-05-13 23:02:51 +0200

    remove deps.

--- crates/libaflmm_cc/src/no-link-rt.c
+++ crates/libaflmm_cc/src/no-link-rt.c
@@ -9,7 +9,7 @@ uint8_t *__afl_area_ptr;
 uint8_t *__afl_acc_memop_ptr;
 
 void __libaflmm_targets_cmplog_instructions(uintptr_t k, uint8_t shape,
-                                          uint64_t arg1, uint64_t arg2) {
+                                            uint64_t arg1, uint64_t arg2) {
   (void)k;
   (void)shape;
   (void)arg1;
@@ -17,8 +17,8 @@ void __libaflmm_targets_cmplog_instructions(uintptr_t k, uint8_t shape,
 }
 
 void __libaflmm_targets_cmplog_instructions_extended(uintptr_t k, uint8_t shape,
-                                                   uint64_t arg1,
-                                                   uint64_t arg2) {
+                                                     uint64_t arg1,
+                                                     uint64_t arg2) {
   (void)k;
   (void)shape;
   (void)arg1;
