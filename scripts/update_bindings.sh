a137f8cb8e34ad3012c5a89ea97b1ac6a788b1dc
Romain Malmain <romain.malmain@pm.me>
2026-05-14 20:39:16 +0200

    Update dependencies to latest version. (#34)
    * update deps to latest versions
    
    * update bindings
    
    * do not inspect aflpp links
    
    * add a cache for nyx
    
    * remove old now useless cache
    
    * install chromium for linkspector, with concurrency checks

--- scripts/update_bindings.sh
+++ scripts/update_bindings.sh
@@ -6,6 +6,6 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 cd "$SCRIPT_DIR/.." || exit 1
 
 # Update LibAFL QEMU bindings
-pushd crates/libafl_qemu
+pushd crates/libaflmm_qemu
   LIBVHARNESS_GEN_STUBS=1 LIBAFL_QEMU_GEN_STUBS=1 cargo +nightly build || exit 1
 popd
