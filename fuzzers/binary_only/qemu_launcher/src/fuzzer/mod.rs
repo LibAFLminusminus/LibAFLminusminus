dd33e34ad716e442b8e9659385fb0da55585f649
Romain Malmain <romain.malmain@pm.me>
2026-05-21 18:29:20 +0200

    forgot some files

---
+++ fuzzers/binary_only/qemu_launcher/src/fuzzer/mod.rs
@@ -0,0 +1,140 @@
+use crate::{
+    fuzzer::profile::QemuProfile,
+    harness::Harness,
+    options::{CommonOptions, FuzzOptions},
+};
+use libaflmm::prelude::*;
+use libaflmm_bolts::OwnedMutSlice;
+use libaflmm_qemu::prelude::*;
+use libaflmm_targets::{EDGES_MAP_DEFAULT_SIZE, MAX_EDGES_FOUND, edges_map_mut_ptr};
+
+mod profile;
+
+pub struct QemuFuzzer;
+
+impl QemuFuzzer {
+    pub fn launch(
+        options: CommonOptions,
+        fuzz_options: FuzzOptions,
+        env: Vec<(String, String)>,
+        args: Vec<String>,
+    ) -> Result<()> {
+        StdLauncher::builder()?
+            .cores(fuzz_options.cores.clone())
+            .timeout(Some(fuzz_options.timeout))
+            .state_builder(move |worker| {
+                let scheduler = QueueScheduler::new();
+                let crash_dir = worker.workdir().create_dir("crashes")?;
+                let queue_dir = worker.workdir().create_dir("queue")?;
+
+                StdState::new(
+                    BytesContext::default(),
+                    // Corpus that will be evolved, we keep it in memory for performance
+                    InMemoryOnDiskCorpus::builder()
+                        .root_dir(queue_dir.as_path())
+                        .scheduler(scheduler)
+                        .build()?,
+                    // Corpus in which we store solutions (crashes in this example),
+                    // on disk so the user can get them after stopping the fuzzer
+                    OnDiskCorpus::<BytesInput, NopScheduler>::builder()
+                        .root_dir(crash_dir.as_path())
+                        .build()?,
+                )
+            })
+            .build_inprocess(move |rt_handle, state| {
+                let core_id = rt_handle.worker().core_id();
+                let profile = QemuProfile::new(&options, &fuzz_options, core_id)?;
+
+                // Create an observation channel using the coverage map
+                let mut edges_observer = unsafe {
+                    HitcountsMapObserver::new(VariableMapObserver::from_mut_slice(
+                        "edges",
+                        OwnedMutSlice::from_raw_parts_mut(
+                            edges_map_mut_ptr(),
+                            EDGES_MAP_DEFAULT_SIZE,
+                        ),
+                        &raw mut MAX_EDGES_FOUND,
+                    ))
+                };
+
+                // Create an observation channel to keep track of the execution time
+                let time_observer = TimeObserver::new("time");
+
+                let map_feedback = StdMapFeedback::with_name("map_feedback", &edges_observer);
+                let map_objective = StdMapFeedback::with_name("map_objective", &edges_observer);
+
+                // Feedback to rate the interestingness of an input
+                // This one is composed by two Feedbacks in OR
+                let feedback = feedback_or!(
+                    // New maximization map feedback linked to the edges observer and the feedback state
+                    map_feedback,
+                    // Time feedback, this one does not need a feedback state
+                    TimeFeedback::new(&time_observer)
+                );
+
+                // A feedback to choose if an input is a solution or not
+                let objective = feedback_and_fast!(
+                    feedback_or_fast!(CrashFeedback::new(), TimeoutFeedback::new()),
+                    map_objective
+                );
+
+                let mut tokens = Tokens::new();
+
+                let injection_module = profile.injection_module(&options)?;
+
+                if let Some(inj) = &injection_module {
+                    for tok in &inj.tokens {
+                        let _ = tokens.add_token(&tok.as_bytes().to_vec());
+                    }
+                }
+
+                if let Some(tokenfile) = &fuzz_options.tokens {
+                    tokens.add_from_file(tokenfile)?;
+                }
+
+                state.metadata_map_mut().insert_unnamed(tokens);
+
+                let modules = profile.get_modules(
+                    &fuzz_options,
+                    &env,
+                    &mut edges_observer,
+                    injection_module,
+                )?;
+
+                let observers = tuple_list!(edges_observer, time_observer);
+
+                let mut emulator = StdEmulator::empty()
+                    .qemu_parameters(args.clone())
+                    .modules(modules)
+                    .build()?;
+
+                let harness = Harness::init(&mut emulator, &options)?;
+
+                let cmplog_observer = profile.cmplog();
+
+                let mut executor = StdQemuExecutor::with_shadow_observers(
+                    state,
+                    emulator,
+                    |state, input, emu| harness.run(state, input, emu),
+                    |_, _, _, _| Ok(()),
+                    observers,
+                    tuple_list!(cmplog_observer),
+                )?;
+
+                let mut stages = tuple_list!(StdStage::default());
+                let mut rand = StdRand::new();
+
+                let mut fuzzer = StdFuzzer::new(
+                    feedback,
+                    objective,
+                    &mut stages,
+                    &mut executor,
+                    state,
+                    rt_handle,
+                )?;
+
+                fuzzer.fuzz_loop(&mut stages, &mut executor, &mut rand, state, rt_handle)
+            })?
+            .launch()
+    }
+}
---
+++ fuzzers/binary_only/qemu_launcher/src/fuzzer/profile.rs
@@ -0,0 +1,144 @@
+use libaflmm::prelude::*;
+use libaflmm_bolts::{CoreId, tuple_list};
+use libaflmm_qemu::prelude::*;
+use std::path::PathBuf;
+
+use crate::options::{CommonOptions, FuzzOptions};
+
+#[derive(Debug)]
+pub struct QemuProfile {
+    pub asan_host: bool,
+    pub asan_guest: bool,
+    pub cmplog: bool,
+    pub injection: bool,
+    pub drcov: Option<PathBuf>,
+}
+
+impl QemuProfile {
+    // resolve options here, to make sure some rules are enforced.
+    pub fn new(common: &CommonOptions, fuzz: &FuzzOptions, core: CoreId) -> Result<Self> {
+        let drcov = common.drcov.clone();
+
+        let asan_host = fuzz.is_asan_host_core(core);
+        let asan_guest = fuzz.is_asan_guest_core(core);
+
+        if asan_host && asan_guest {
+            return Err(illegal_argument!(
+                "A core cannot be both asan-host and asan-guest"
+            ));
+        }
+
+        let cmplog = fuzz.is_cmplog_core(core);
+        let injection = common.injections.is_some();
+
+        Ok(Self {
+            asan_host,
+            asan_guest,
+            drcov,
+            cmplog,
+            injection,
+        })
+    }
+
+    fn build_asan_filter(&self, opt: &FuzzOptions) -> StdAddressFilter {
+        if let Some(include_asan) = &opt.include_asan {
+            log::info!("ASAN includes: {include_asan:#x?}");
+            StdAddressFilter::allow_list(include_asan.to_vec())
+        } else if let Some(exclude_asan) = &opt.exclude_asan {
+            log::info!("ASAN excludes: {exclude_asan:#x?}");
+            StdAddressFilter::deny_list(exclude_asan.to_vec())
+        } else {
+            log::info!("ASAN no additional filter");
+            StdAddressFilter::default()
+        }
+    }
+
+    #[cfg(feature = "injections")]
+    pub fn injection_module(&self, opt: &CommonOptions) -> Result<Option<InjectionModule>> {
+        if !self.injection {
+            return Ok(None);
+        }
+
+        let injections_file = opt.injections.as_ref().unwrap();
+        let extension = injections_file.extension().unwrap().to_str().unwrap();
+
+        let module = if extension == "yaml" || extension == "yml" {
+            InjectionModule::from_yaml(injections_file).unwrap()
+        } else if extension == "toml" {
+            InjectionModule::from_toml(injections_file).unwrap()
+        } else {
+            return Err(illegal_argument!(
+                "The injection file must have a yaml,yml or toml extension."
+            ));
+        };
+
+        Ok(Some(module))
+    }
+
+    #[cfg(not(feature = "injections"))]
+    pub fn injection_module(&self, _opt: &CommonOptions) -> Result<Option<InjectionModule>> {
+        Ok(None)
+    }
+
+    pub fn cmplog(&self) -> Option<StdCmpLogObserver> {
+        self.cmplog.then(|| CmpLogObserver::new("CmpLog", true))
+    }
+
+    pub fn get_modules<I, O, S>(
+        &self,
+        fuzz: &FuzzOptions,
+        env: &[(String, String)],
+        edges_observer: &mut O,
+        injection_module: Option<InjectionModule>,
+    ) -> Result<impl EmulatorModuleTuple<I, S> + use<I, O, S>>
+    where
+        I: Unpin,
+        O: VarLenMapObserver,
+        S: State<Input = I> + Unpin,
+    {
+        let edge_coverage_module = StdEdgeCoverageModule::builder()
+            .map_observer(edges_observer.as_mut())
+            .build()?;
+
+        let mut snapshot_module = SnapshotModule::with_filters(AsanGuestModule::snapshot_filters());
+
+        if !fuzz.snapshots || fuzz.iterations.is_some() {
+            snapshot_module.use_manual_reset();
+        }
+
+        let drcov_module = self
+            .drcov
+            .as_ref()
+            .map(|p| DrCovModule::builder().path(p).full_trace(true).build());
+
+        let asan_host_module = self.asan_host.then(|| {
+            let asan_filter = self.build_asan_filter(fuzz);
+
+            unsafe {
+                AsanHostModule::builder()
+                    .env(env)
+                    .filter(asan_filter)
+                    .asan_report()
+                    .build()
+            }
+        });
+
+        let asan_guest_module = self.asan_guest.then(|| {
+            let asan_filter = self.build_asan_filter(fuzz);
+
+            AsanGuestModule::new(env, asan_filter)
+        });
+
+        let cmplog_module = self.cmplog.then(CmpLogModule::default);
+
+        Ok(tuple_list!(
+            edge_coverage_module,
+            snapshot_module,
+            drcov_module,
+            asan_host_module,
+            asan_guest_module,
+            cmplog_module,
+            injection_module
+        ))
+    }
+}
