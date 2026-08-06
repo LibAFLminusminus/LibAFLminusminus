36a480e2e180ab079f46733fdf8305926e33036c
Romain Malmain <romain.malmain@pm.me>
2026-08-06 15:46:59 +0200

    fmt

--- fuzzers/binary_only/frida_libpng/src/fuzzer.rs
+++ fuzzers/binary_only/frida_libpng/src/fuzzer.rs
@@ -5,30 +5,30 @@ use core::time::Duration;
 use frida_gum::Gum;
 use libaflmm::{
     controllers::{StdController, Worker},
-    corpus::{schedulers::QueueScheduler, InMemoryCorpus, ObjectiveOnDiskCorpus},
+    corpus::{InMemoryCorpus, ObjectiveOnDiskCorpus, schedulers::QueueScheduler},
     executors::ExitKind,
     feedback_or, feedback_or_fast,
     feedbacks::{CrashFeedback, MaxMapFeedback, TimeFeedback, TimeoutFeedback},
     fuzzers::{Fuzzer, Loader, StdFuzzer},
     inputs::{BytesContext, BytesInput, InputContext},
-    launchers::{groups::StdGroup, StdLauncher},
+    launchers::{StdLauncher, groups::StdGroup},
     monitors::WebMonitor,
     mutators::{
         havoc_mutations::havoc_mutations,
-        scheduled::{tokens_mutations, HavocScheduledMutator},
+        scheduled::{HavocScheduledMutator, tokens_mutations},
         token_mutations::I2SRandReplace,
     },
     observers::{CmpLogObserver, HitcountsMapObserver, StdMapObserver, TimeObserver},
     stages::{
-        cmplog_post_hook, cmplog_pre_hook, constrain, IfElseStage, SingleRunStage,
-        StdMutationalStage,
+        IfElseStage, SingleRunStage, StdMutationalStage, cmplog_post_hook, cmplog_pre_hook,
+        constrain,
     },
     states::{State, StdState},
 };
 use libaflmm_bolts::{
-    rands::StdRand,
-    tuples::{tuple_list, Merge},
     FastTimer, Result,
+    rands::StdRand,
+    tuples::{Merge, tuple_list},
 };
 use libaflmm_frida::{
     asan::{
